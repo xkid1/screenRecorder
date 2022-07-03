64a41a3f4e7760625e114fcca1330f004748b286
xkid1 <elmer.alluad@gmail.com>
2022-07-03 09:48:25 +0800

    remove comment

--- src/plugins/background.ts
+++ src/plugins/background.ts
@@ -1,18 +1,1 @@
-// chrome.runtime.onInstalled.addListener(async () => {
-//     let url = chrome.runtime.getURL('index.html');
 
-//     //Open new window with the background page
-//     // await chrome.windows.create({
-//     //     url: url,
-//     //     type: 'popup',
-//     //     width: 300,
-//     //     height: 300,
-//     //     left: screen.width / 2 - 150,
-//     //     top: screen.height / 2 - 150,
-//     // });
-
-//     await chrome.tabs.create({
-//         url,
-//     });
-
-// });
